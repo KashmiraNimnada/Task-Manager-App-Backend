c3bf99d7bd827b6e6d890ad49fdce5c1ab0a59ae
KashmiraNimnada <kashmiranimnada98@gmail.com>
2024-11-12 23:40:16 +0530

    Some changes done

--- src/main/java/com/asign/taskm/TaskmApplication.java
+++ src/main/java/com/asign/taskm/TaskmApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmApplication.class, args);
 	}
-
 }
--- src/main/java/com/asign/taskm/controller/TaskController.java
+++ src/main/java/com/asign/taskm/controller/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.asign.taskm.service.TaskService;
 import com.asign.taskm.service.UserService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TaskController {
 
     @Autowired
@@ -29,7 +31,6 @@ public class TaskController {
 
     @PostMapping("/tasks")
     public ResponseEntity<Task> createTask(@RequestBody TaskDto taskDto){
-
         Task task = new Task();
         task.setName(taskDto.getName());
         task.setDescription(taskDto.getDescription());
